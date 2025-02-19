570188bccba4f476ee7005641dc47792b084f243
Alexander Ng <ng.alexander1031@gmail.com>
2025-02-19 06:39:18 -0600

    added structs

--- main.go
+++ main.go
@@ -1,8 +1,49 @@
 package main
 
 import (
-  "encoding/json"
-  "log"
-  "math/rand"
-  "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+type Receipt struct {
+	Retailer     string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Items        []Item `json:"items"`
+	Total        string `json:"total"`
+}
+
+type Item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
+}
+
+type ProcessResponse struct {
+	ID string `json:"id"`
+}
+
+type PointsResponse struct {
+	Points int `json:"points"`
+}
+
+func processReceiptsHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getPointsHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/receipts/process", processReceiptsHandler).Methods("POST")
+	r.HandleFunc("/receipts/{id}/points", getPointsHandler).Methods("GET")
+	fmt.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", r)
+}
