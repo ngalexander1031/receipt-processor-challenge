bfa9590d077465ab47de44f23dcc5f21d61187a9
Alexander Ng <ng.alexander1031@gmail.com>
2025-02-20 15:41:01 -0600

    added points logic

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -33,8 +37,36 @@ type PointsResponse struct {
 	Points int `json:"points"`
 }
 
-func calculatePoints() int {
+func calculatePoints(r Receipt) int {
 	points := 0
+	for _, char := range r.Retailer {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9') {
+			points += 1
+		}
+	}
+	total, _ := strconv.ParseFloat(r.Total, 64)
+	if math.Mod(total, 1) == 0 {
+		points += 50
+	}
+	if math.Mod(total, 0.25) == 0 {
+		points += 25
+	}
+	points += 5 * (len(r.Items) / 2)
+	for _, item := range r.Items {
+		trimmed := strings.TrimSpace(item.ShortDescription)
+		if len(trimmed)%3 == 0 {
+			price, _ := strconv.ParseFloat(item.Price, 64)
+			points += int(math.Ceil(price * 0.2))
+		}
+	}
+	purchaseDate, _ := time.Parse("2006-01-02", r.PurchaseDate)
+	purchaseTime, _ := time.Parse("15:04", r.PurchaseTime)
+	if purchaseDate.Day()%2 == 1 {
+		points += 6
+	}
+	if purchaseTime.Hour() >= 14 && purchaseTime.Hour() < 16 {
+		points += 10
+	}
 
 	return points
 }
